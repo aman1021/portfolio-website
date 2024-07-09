82d29fdfc1d7dac4fba0518999ee70eddf7907a1
aman1021 <amaniiitu32@gmail.com>
2024-07-09 18:10:21 +0530

    small changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,7 +31,7 @@ const NavBar = () => {
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-100">
       <div className="flex flex-wrap items-center justify-between mx-auto p-6">
         <Link href={"/"} className="text-5xl text-white font-semibold">
-        卂爪卂几
+        ΛMΛП
         </Link>
         <div className="mobile-menu block md:hidden">
           {!navBarOpen ? (
--- src/components/ProjectsCards.jsx
+++ src/components/ProjectsCards.jsx
@@ -12,12 +12,14 @@ const ProjectsCards = ({ imgUrl, title, description, gitUrl, liveUrl }) => {
         <div className="overlay  items-center justify-center absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden group-hover:flex group-hover:bg-opacity-80 transition-all duration-500">
           <Link
             href={gitUrl}
+            title="Github Url"
             className="h-14 w-14 mr-4 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <CodeBracketIcon className="h-10 w-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#ADB7BE]  cursor-pointer group-hover/link:text-white" />
           </Link>
           <Link
             href={liveUrl}
+            title="Live Url"
             className="h-14 w-14 border-2 relative rounded-full border-[#ADB7BE] hover:border-white group/link"
           >
             <EyeIcon className="h-10 w-10 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-[#ADB7BE]  cursor-pointer group-hover/link:text-white" />
