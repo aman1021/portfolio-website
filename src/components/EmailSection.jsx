d0636d0c495abc775758f3679f147cf27b901cef
aman1021 <amaniiitu32@gmail.com>
2024-08-30 19:59:37 +0530

    added links

--- src/components/EmailSection.jsx
+++ src/components/EmailSection.jsx
@@ -65,10 +65,10 @@ const EmailSection = () => {
           try my best to get back to you!
         </p>
         <div className="socials flex flex-row gap-2">
-          <Link href="https://github.com">
+          <Link href="https://github.com/aman1021">
             <Image src={GithubIcon} alt="Github Icon" />
           </Link>
-          <Link href="https://linkedin.com">
+          <Link href="https://www.linkedin.com/in/amanchandel/">
             <Image src={LinkedinIcon} alt="Linkedin Icon" />
           </Link>
         </div>
